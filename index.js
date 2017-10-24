fddab844058bb6be3aee5a995503b27b23b1292f
Gary Bunofsky <gary@garybunofsky.com>
2017-10-23 22:19:16 -0400

    Created layout skeleton

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = 8080;
+
+
+app.use('/public', express.static('public'))
 
 app.use('/', function (req, res){
   res.sendFile(path.join(__dirname + '/index.html'));
-})
+});
 
-
-app.listen(8080);
+app.listen(port);
+console.log('Express running on port ' + port);
