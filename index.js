466a839d0c58cdb0ee06bb94b9892c94d964b994
Gary Bunofsky <gary@garybunofsky.com>
2017-10-24 17:26:25 -0400

    Removed static port from index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var port = 8080;
 
 
 app.use('/public', express.static('public'))
@@ -10,5 +9,5 @@ app.use('/', function (req, res){
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(port);
-console.log('Express running on port ' + port);
+.listen(process.env.PORT || 5000)
+console.log('Express running');
